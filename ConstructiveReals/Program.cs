5c721dba545f4af5ea550123c649808dbe3009a3
Nico Michaelis <86128620+nicomichaelis@users.noreply.github.com>
2022-10-28 17:02:19 +0200

    Add projects

---
+++ ConstructiveReals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConstructiveReals;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        return 0;
+    }
+}
